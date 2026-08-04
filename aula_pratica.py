2afcba6470b7e6e72aff8c4521e31a52216b61c6
BrundoCJ <bconrado04@gmail.com>
2026-08-04 15:49:17 -0300

    atualizando atividade

--- aula_pratica.py
+++ aula_pratica.py
@@ -1,7 +1,14 @@
-print("Hello, World!")    
+print("Hello, World!")
 
-numero =  input("Digite um número: ")
+continuar = "s"
 
-for i in range (1, 11):
-    resultado = int(numero) * i
-    print(f"{numero} x {i} = {resultado}")
+while continuar == "s":
+    numero = input("Digite um número: ")
+
+    for i in range(1, 11):
+        resultado = int(numero) * i
+        print(f"{numero} x {i} = {resultado}")
+
+    continuar = input("Deseja digitar outro número? (s/n): ")
+
+print("Programa encerrado.")
